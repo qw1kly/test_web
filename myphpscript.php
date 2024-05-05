de8d6e25e6ba6885b6696acb52bf6551f1605aa2
qw1kly <168624106+qw1kly@users.noreply.github.com>
2024-05-05 17:31:58 +0300

    Update myphpscript.php

--- myphpscript.php
+++ myphpscript.php
@@ -1,9 +1,30 @@
 <?php
     if (isset($_POST['myActionName'])
     {
-        $fd = fopen("hello.txt", 'w') or die("не удалось создать файл");
-        $str = "Привет мир";
-        fwrite($fd, $str);
-        fclose($fd);
+        define('TELEGRAM_TOKEN', '6719114253:AAEGvzdBuqPjMbVJtlP_UUYiSbw-uKbeghU');
+ 
+        // сюда нужно вписать ваш внутренний айдишник
+        define('TELEGRAM_CHATID', '457835158');
+         
+        message_to_telegram('Привет!');
+         
+        function message_to_telegram($text)
+        {
+            $ch = curl_init();
+            curl_setopt_array(
+                $ch,
+                array(
+                    CURLOPT_URL => 'https://api.telegram.org/bot' . TELEGRAM_TOKEN . '/sendMessage',
+                    CURLOPT_POST => TRUE,
+                    CURLOPT_RETURNTRANSFER => TRUE,
+                    CURLOPT_TIMEOUT => 10,
+                    CURLOPT_POSTFIELDS => array(
+                        'chat_id' => TELEGRAM_CHATID,
+                        'text' => $text,
+                    ),
+                )
+            );
+            curl_exec($ch);
+}
     }
 ?>
