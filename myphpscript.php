5f85c83caea34ca919dc7ba3256c2a6473567dc5
qw1kly <168624106+qw1kly@users.noreply.github.com>
2024-05-06 00:02:11 +0300

    Update myphpscript.php

--- myphpscript.php
+++ myphpscript.php
@@ -1,30 +1,11 @@
 <?php
-    if (isset($_POST['myActionName'])
-    {
-        define('TELEGRAM_TOKEN', '6719114253:AAEGvzdBuqPjMbVJtlP_UUYiSbw-uKbeghU');
- 
-        // сюда нужно вписать ваш внутренний айдишник
-        define('TELEGRAM_CHATID', '457835158');
-         
-        message_to_telegram('Привет!');
-         
-        function message_to_telegram($text)
-        {
-            $ch = curl_init();
-            curl_setopt_array(
-                $ch,
-                array(
-                    CURLOPT_URL => 'https://api.telegram.org/bot' . TELEGRAM_TOKEN . '/sendMessage',
-                    CURLOPT_POST => TRUE,
-                    CURLOPT_RETURNTRANSFER => TRUE,
-                    CURLOPT_TIMEOUT => 10,
-                    CURLOPT_POSTFIELDS => array(
-                        'chat_id' => TELEGRAM_CHATID,
-                        'text' => $text,
-                    ),
-                )
-            );
-            curl_exec($ch);
+if ($_POST['action'] == 'callThisFunction') {
+    // Вызываем функцию
+    echo executeFunction();
 }
-    }
+
+function executeFunction() {
+    // Здесь находится тело вашей функции
+    return 'Функция успешно выполнена!';
+}
 ?>
