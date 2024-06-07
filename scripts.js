00d9126001d4a06df0b8488c2c9657b3aa89a579
qw1kly <168624106+qw1kly@users.noreply.github.com>
2024-06-07 22:22:12 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 function Myfunc() {
             var image = document.getElementById("gifanim");
             image.src = "https://sun1-13.userapi.com/c825102/u607470817/docs/d1/13bc9e9189d3/video.mp4?extra=BgnLL_vwkXmmooXql_vHUmaAbzCSwU9bd0sRZZisLwOgpHP3uTSIxO9k5ALbsUvuvrakdn143zJerCwEzPFnwIRrBOoVuJB4N2sq0yROiNblMC2znGchpw3aaV2r_hjlycV_-lnEylUKwhzw5SczRLlu";
-            }
+            document.getElementById("inv").style.display="none";
+            
+}
