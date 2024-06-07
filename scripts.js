34a2ebe47f781b0cd4cb511b2f8b11ebfc70215b
qw1kly <168624106+qw1kly@users.noreply.github.com>
2024-06-07 22:07:49 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 function Myfunc() {
             document.getElementById("inv").style.display="none";
-            document.getElementByClassName("basket").src = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Rotating_earth_%28large%29.gif/200px-Rotating_earth_%28large%29.gif";
+            document.getElementById("gifanim").src = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Rotating_earth_%28large%29.gif/200px-Rotating_earth_%28large%29.gif";
             }
