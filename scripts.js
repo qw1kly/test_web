bbd04b236acf522462d415d6e6c39b5b5d358cb8
qw1kly <168624106+qw1kly@users.noreply.github.com>
2024-06-07 21:54:06 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
 function Myfunc() {
             document.getElementById("inv").style.display="none";
+            document.getElementByClassName("basket").src = "https://vk.com/doc607470817_676427640?hash=GrqKIKUGzub53p2X6R1RTrJvatEZFP4MvFuK8m3nM7g&dl=OOXOhziPZOw243RKmZMghU38tzEDUGpGoIQQNBMQjj0&wnd=1&module=im";
             }
