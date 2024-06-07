b821bce270ce5c516d16d534127759c7d7b8d8f1
qw1kly <168624106+qw1kly@users.noreply.github.com>
2024-06-07 22:13:09 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 function Myfunc() {
-            document.getElementById("inv").style.display="none";
             var image = document.getElementById("gifanim");
             image.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Rotating_earth_%28large%29.gif/200px-Rotating_earth_%28large%29.gif";
             }
