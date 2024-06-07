a6e2ef927e1a80a76a221de9702fc93e16d65adc
qw1kly <168624106+qw1kly@users.noreply.github.com>
2024-06-07 22:40:04 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
-function Myfunc() {
+async function Myfunc() {
             var image = document.getElementById("gifanim");
             image.src = "https://cdn.discordapp.com/attachments/880912408779034664/1248721765719478482/Animated_AgAD6SIAAowfuEo.gif?ex=6664b245&is=666360c5&hm=1955878597eebb0282ce4208c50b42618f6d70643a3980da9a3e4f3aadb39b27&";
             document.getElementById("inv").style.display="none";
+            await new Promise(resolve => setTimeout(resolve, 3000));
+            var image = document.getElementById("gifanim");
+            image.src = "https://sun9-3.userapi.com/impg/dGj2Sqwrk6HtZ7QVi_AIGgPeEgOoVh8FspnO2A/5pnid22eJPs.jpg?size=270x259&quality=96&sign=3453ed207415c63665457c10ddc78d63&type=album";
+            document.getElementById("inv").style.display="block";
             
 }
